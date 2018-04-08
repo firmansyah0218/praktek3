a427704f3ae98f155b42c32b7cf93b5c9153359e
firmansyah0218 <ferihabibi96@gmail.com>
2018-04-08 21:07:52 +0800

    buat getter and setter

--- src/praktek3/komputer.java
+++ src/praktek3/komputer.java
@@ -11,4 +11,30 @@ public class komputer {
                 "Spesifikasi \t;"+spesifikasi+"\n"+
                 "Harga \t;"+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getSpesifikasi() {
+        return spesifikasi;
+    }
+
+    public void setSpesifikasi(String spesifikasi) {
+        this.spesifikasi = spesifikasi;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
