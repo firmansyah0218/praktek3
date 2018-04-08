b3d8b10d51448558f46b8ee9d3199e7248087e85
firmansyah0218 <ferihabibi96@gmail.com>
2018-04-08 21:03:54 +0800

    instance , buat atribut dan panggil method

--- src/praktek3/komputeraksi.java
+++ src/praktek3/komputeraksi.java
@@ -2,6 +2,11 @@ package praktek3;
 
 public class komputeraksi {
     public static void main(String[] args) {
+        komputer k = new komputer();
+        k.merk="acer";
+        k.spesifikasi="intel inside, 3gb";
+        k.harga=4000000;
         
+        k.cetakInfo();
     }
 }
