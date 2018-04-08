a0a34603b7300de469a17cab85a023d388bf2ce0
firmansyah0218 <ferihabibi96@gmail.com>
2018-04-08 20:59:11 +0800

    method cetakinfo()

--- src/praktek3/komputer.java
+++ src/praktek3/komputer.java
@@ -4,4 +4,11 @@ public class komputer {
     String merk;
     String spesifikasi;
     double harga;
+    
+    
+    void cetakInfo(){
+        System.out.println("Merk \t;"+merk+"\n"+
+                "Spesifikasi \t;"+spesifikasi+"\n"+
+                "Harga \t;"+harga);
+    }
 }
